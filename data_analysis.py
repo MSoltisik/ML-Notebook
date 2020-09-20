26a032cc30b9c087343a680268a1741c6e64f8f6
MSoltisik <71102637+MSoltisik@users.noreply.github.com>
2020-09-20 23:18:28 +0200

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -139,7 +139,7 @@ st.write("Base Data:")
 st.write(data)
 
 # Showing the label distribution among testing and training data
-display_label_dist = st.checkbox("Display label distribution in the chosen dataset)
+display_label_dist = st.checkbox("Display label distribution in the chosen dataset")
 if display_label_dist:
     st.markdown("Data label distribution:")
     st.bar_chart(data["attack_type"])
